030875b5292b908f3d374daec674fb9d1204b8b6
Asmin Paudel <asminpaudel@gmail.com>
2022-08-16 18:07:00 +0545

    Update SendMessage.py

--- scripts/SendMessage.py
+++ scripts/SendMessage.py
@@ -1,13 +1,11 @@
 from discord import Webhook, RequestsWebhookAdapter
 
-
 webhook_url = "https://discord.com/api/webhooks/987378652813623367/splN2-ePyKsx4UM2BY11jKNIhMh7MABEBdPpaEjQX_RLeEmZM5ya7o1ed_IkFYhBBh3o"
 
 url_Stochastic_RSI_webhook = "https://discord.com/api/webhooks/1008748730494763130/PdaWZK6AS8xb3zUDjTm66fAnOw5jjXOx8EvHvJ7ZfSzZhxhdTv965K3qaDwONJYwc91q"
 url_Bollinger_Band_webhook = "https://discord.com/api/webhooks/1008748991804080278/GAn9cfQemeWLzLJTh3LgkJIZHG4OyKS9ccageEauCsykrSMzt0uqpbLKiKl2qPl1FJe9"
 
 webhook = Webhook.from_url(webhook_url, adapter=RequestsWebhookAdapter())
-# webhook.send("Hello World")
 
 
 def send_BollingerBand_message(buylist, selllist):
