59abd5f4bedd11bd23da4b564a42e7aff4e0de86
Asmin Paudel <asminpaudel@gmail.com>
2022-08-30 13:08:32 +0545

    Update SendMessage.py
    use secret keys

--- scripts/SendMessage.py
+++ scripts/SendMessage.py
@@ -1,10 +1,12 @@
 import discord
 from discord import Webhook, RequestsWebhookAdapter
+import os
+
+webhook_url = os.environ['WEBHOOK_URL']
+url_Stochastic_RSI_webhook = os.environ['WEBHOOK_STOCHASTIC_RSI ']
+url_Bollinger_Band_webhook = os.environ['WEBHOOK_BOLLINGER_BAND ']
 
 
-webhook_url = "https://discord.com/api/webhooks/987378652813623367/splN2-ePyKsx4UM2BY11jKNIhMh7MABEBdPpaEjQX_RLeEmZM5ya7o1ed_IkFYhBBh3o"
-url_Stochastic_RSI_webhook = "https://discord.com/api/webhooks/1008748730494763130/PdaWZK6AS8xb3zUDjTm66fAnOw5jjXOx8EvHvJ7ZfSzZhxhdTv965K3qaDwONJYwc91q"
-url_Bollinger_Band_webhook = "https://discord.com/api/webhooks/1008748991804080278/GAn9cfQemeWLzLJTh3LgkJIZHG4OyKS9ccageEauCsykrSMzt0uqpbLKiKl2qPl1FJe9"
 webhook = Webhook.from_url(webhook_url, adapter=RequestsWebhookAdapter())
 
 
